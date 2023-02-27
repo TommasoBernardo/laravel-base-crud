6e829fb0fce3c3d15bb032695319db65e14f1d18
TommasoBernardo <tommasob1210@gmail.com>
2023-02-27 18:44:00 +0100

    add url image

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,5 +9,10 @@ class Book extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['isbn','title','author','publication_date','description', 'genre','cover_image'];
+    protected $fillable = ['isbn', 'title', 'author', 'publication_date', 'description', 'genre', 'cover_image'];
+
+    public function isImageAUrl()
+    {
+        return filter_var($this->cover_image, FILTER_VALIDATE_URL);
+    }
 }
--- resources/views/admin/books/show.blade.php
+++ resources/views/admin/books/show.blade.php
@@ -1,47 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card text-center mt-5">
-        <div class="card-header">
-            <p>
-                ISBN: {{ $book->isbn }}
-            </p>
-            <h3>
-                {{ $book->author }}
-            </h3>
-        </div>
-        <div class="card-body p-3 m-3">
-            <h2 class="card-title fw-bold p-3">
-                {{ $book->title }}
-            </h2>
-            <p class="">
-                Genre: {{ $book->genre }}
-            </p>
-            <p class="card-text mb-4">
-                {{ $book->description }}
-            </p>
-            <p class="card-text mb-4">
-                {{ $book->publication_date }}
-            </p>
-            <a href="{{ route('admin.books.index', $book->id) }}" class="btn btn-primary">
-                Index
-            </a>            
-            <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-success">
-                Edit
-            </a>
-            <form action="{{ route('admin.books.destroy', $book->id) }}" method="POST" class="d-inline-block">
-                @csrf
-                @method('DELETE')
+    <div class="container">
+        <div class="card text-center mt-5">
+            <div class="card-header">
+                <p>
+                    ISBN: {{ $book->isbn }}
+                </p>
+                <h3>
+                    {{ $book->author }}
+                </h3>
+            </div>
+            <div class="card-body p-3 m-3">
+                <h2 class="card-title fw-bold p-3">
+                    {{ $book->title }}
+                </h2>
+                <p class="">
+                    Genre: {{ $book->genre }}
+                </p>
+                <p class="card-text mb-4">
+                    {{ $book->description }}
+                </p>
+                <p class="card-text mb-4">
+                    {{ $book->publication_date }}
+                </p>
 
-                <button type="submit" class="btn btn-danger">
-                    Delete
-                </button>
-            </form>
+                <div class="card-image mb-4">
+                    @if ($book->isImageAUrl())
+                    <img src="{{ $book->cover_image }}" @else <img src="{{ asset('storage/' . $book->cover_image) }}"
+                            @endif
+                        alt="{{ $book->title }} image" class="img-fluid">
+                </div>
+
+                <a href="{{ route('admin.books.index', $book->id) }}" class="btn btn-primary">
+                    Index
+                </a>
+                <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-success">
+                    Edit
+                </a>
+                <form action="{{ route('admin.books.destroy', $book->id) }}" method="POST" class="d-inline-block">
+                    @csrf
+                    @method('DELETE')
+
+                    <button type="submit" class="btn btn-danger">
+                        Delete
+                    </button>
+                </form>
+            </div>
+            <div class="card-footer text-muted">
+                {{ $book->book_date }}
+            </div>
         </div>
-        <div class="card-footer text-muted">
-            {{ $book->book_date }}
-        </div>
-      </div>
-</div>
+    </div>
 @endsection
